22e14dbad91c5f873e474332e9433cf58bace705
R2BT <theeraphat.a@ku.th>
2021-03-15 17:58:44 +0700

    เกมทายตัวเลข_6320500573

---
+++ เกมทายตัวเลข.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int push(int *n1,int *n2)
+{
+    printf("%d",n1);
+
+
+}
+int main()
+{
+    int n,num1,num2;
+    scanf("%d",&n);
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    push(&num1,&num2);
+
+
+
+
+
+
+
+
+    return 0;
+}
